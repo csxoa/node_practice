35d951a69b44c40fbe9a9a1d54389759a3084efc
sue517 <csooa9305@gmail.com>
2021-02-22 00:30:24 +0900

    session 03-2 module

--- node_blogs/app.js
+++ node_blogs/app.js
@@ -8,4 +8,11 @@ app.use(express.json()) // app.use 는 순차적으로 실행된다.
 app.use(logger)
 app.use(routes) // routes directory 의 router 들을 실행한다.
 
+// general error handler
+app.use((err, req, res, next) => {
+    const { statusCode, message } = err
+    console.error(err)
+    res.status(statusCode).json({ message })
+}) // middleware 함수들의 인자가 네개일경우 첫번째 인자는 무조건 에러
+
 module.exports = app
